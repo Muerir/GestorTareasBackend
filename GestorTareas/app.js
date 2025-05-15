2330501c6e9459ecfc4e134c9203d83726599fbe
Muerir <47600456+Muerir@users.noreply.github.com>
2025-05-15 14:22:14 -0600

    Se comentaron las lineas en conflicto con render

--- GestorTareas/app.js
+++ GestorTareas/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api/usuario', require('./routes/usuario.routes'));
 app.use('/api/tareas', require('./routes/tarea.routes'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
 
 module.exports = app;
